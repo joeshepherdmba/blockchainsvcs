d69665a7bbff1f914b671e9e051a4367daa5c7ef
joeshepherdmba <joeshepherdmba@outlook.com>
2018-01-30 13:00:21 -0600

    initial loan contract

--- src/contracts/loan.sol
+++ src/contracts/loan.sol
@@ -1,3 +1,25 @@
+pragma solidity ^0.4.16;
+
+contract Loan {
+    enum State { New, Active, Declined, Paid, Default }
+    address debtor;
+    address lender;
+    uint loanAmount;
+    uint interestRate;
+    uint months;
+    State public state; 
+
+    function CreateLoan(address debtor, address lender, uint amount) public payable {
+        debtor = debtor; 
+        lender = lender;
+        loanAmount = amount;
+    }
+
+    event LoanApproved();
+    event PaidInFull();
+    event Defaulted();
+    event Declined();
+}
 // set terms
 
 // transfer funds
