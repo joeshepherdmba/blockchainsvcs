273ca421acd468361c6d6d98c820c127a7cefeeb
joeshepherdmba <joeshepherdmba@outlook.com>
2018-01-30 16:23:46 -0600

    formatted contract for readability

--- src/contracts/loan.sol
+++ src/contracts/loan.sol
@@ -10,53 +10,64 @@ contract Loan {
     enum State { New, Active, Declined, Paid, Default }
     State public state; 
 
-    mapping(address => uint) public balances;
+    mapping(address => uint) loanBalances;
 
+    // Set up new loan
+    function Loan(address debtor, uint rate) public {
+        _lender = msg.sender;
+        _debtor = debtor;
+        interestRate = rate;
+        New(_lender, _debtor, interestRate);
+    }
+
+    // Fund new loan
+    function fund(address debtor) public payable onlyLender {
+        loanAmount = msg.value;
+  
+        _debtor = debtor; 
+
+        _debtor.transfer(loanAmount);
+        loanBalances[_debtor] += loanAmount;
+
+        Funded(msg.sender, debtor, loanAmount);
+    }
+
+    // Make payment
+    function makePayment(address lender, uint date) public payable onlyDebtor {
+        _lender.transfer(msg.value);
+        loanBalances[_debtor] -= loanAmount;
+
+        if (loanBalances[_debtor] == 0) {
+            PaidInFull(msg.sender, lender, msg.value, date);
+        } else {
+            PaymentMade(msg.sender, lender, msg.value, date, loanBalances[_debtor]);
+        }
+    }
+
+
+    /*
+     Modifiers
+    */
+
+    // Require Lender is sender
     modifier onlyLender() {
         require(msg.sender == _lender);
         _;
     }
-
+    
+    // Require Debtor is sender
     modifier onlyDebtor() {
         require(msg.sender == _debtor);
         _;
     }
 
-    function Loan(address debtor, uint rate) public {
-        _lender = msg.sender;
-        _debtor = debtor;
-        interestRate = rate;
-    }
+    /*
+     Events
+    */
 
-    function fund(address debtor) public payable onlyLender {
-        loanAmount = msg.value;
-        
-        if (balances[msg.sender] < loanAmount) 
-        return;
-        
-        _debtor = debtor; 
-        balances[msg.sender] -= loanAmount;
-        balances[debtor] += loanAmount;
-        Funded(msg.sender, debtor, loanAmount);
-    }
-
-    function payOff(address lender, uint amount, uint date) public payable onlyDebtor {
-        if (balances[msg.sender] < amount) 
-        return;
-        
-        balances[msg.sender] -= amount;
-        balances[lender] += amount;
-        PaidInFull(msg.sender, lender, amount, date);
-    }
-
+    event New(address lender, address debtor, uint rate);
     event Funded(address lender, address debtor, uint amount);
     event PaidInFull(address debtor, address lender, uint amount, uint datePaid);
+    event PaymentMade(address debtor, address lender, uint amount, uint datePaid, uint balance);
     event Defaulted();
 }
-// set terms
-
-// transfer funds
-
-// accept payment
-
-// close loan
